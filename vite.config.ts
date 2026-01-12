c2566507fce11d8566cb76c760b5418afdbbcfa5
Link <mrchengzp@qq.com>
2026-01-12 19:32:59 +0800

    chore: deploy to pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/shares-web/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
   }
-})
+}))
 
